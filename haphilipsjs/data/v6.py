a2711aa592f45274a5f93bd09b33fa722560af80
Joakim Plate <elupus@ecce.se>
2021-02-05 00:28:39 +0100

    Add some application data

--- haphilipsjs/data/v6.py
+++ haphilipsjs/data/v6.py
@@ -39,6 +39,194 @@ SYSTEM = {
 
 VOLUME = {"muted": False, "current": 18, "min": 0, "max": 60}
 
+APPLICATIONS = {
+    "version": 0,
+    "applications": [
+        {
+            "label": "Så här......",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.eum",
+                    "className": "org.droidtv.eum.onehelp.HowToTutorials.HowToVideosActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.eum.onehelp.HowToTutorials.HowToVideosActivity-org.droidtv.eum",
+            "type": "app",
+        },
+        {
+            "label": "Play Butik",
+            "intent": {
+                "component": {
+                    "packageName": "com.android.vending",
+                    "className": "com.google.android.finsky.tvmainactivity.TvMainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "com.google.android.finsky.tvmainactivity.TvMainActivity-com.android.vending",
+            "type": "app",
+        },
+        {
+            "label": "YouTube",
+            "intent": {
+                "component": {
+                    "packageName": "com.google.android.youtube.tv",
+                    "className": "com.google.android.apps.youtube.tv.activity.ShellActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "com.google.android.apps.youtube.tv.activity.ShellActivity-com.google.android.youtube.tv",
+            "type": "app",
+        },
+        {
+            "label": "TED",
+            "intent": {
+                "component": {
+                    "packageName": "com.ted.android.tv",
+                    "className": "com.ted.android.tv.view.MainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "com.ted.android.tv.view.MainActivity-com.ted.android.tv",
+            "type": "app",
+        },
+        {
+            "label": "Play Spel",
+            "intent": {
+                "component": {
+                    "packageName": "com.google.android.play.games",
+                    "className": "com.google.android.apps.play.games.app.atv.features.home.HomeActivity",
+                },
+                "action": "empty",
+            },
+            "order": 0,
+            "id": "com.google.android.apps.play.games.app.atv.features.home.HomeActivity-com.google.android.play.games",
+            "type": "app",
+        },
+        {
+            "label": "Netflix",
+            "intent": {
+                "component": {
+                    "packageName": "com.netflix.ninja",
+                    "className": "com.netflix.ninja.MainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "com.netflix.ninja.MainActivity-com.netflix.ninja",
+            "type": "app",
+        },
+        {
+            "label": "Amazon Alexa",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.amazonalexa",
+                    "className": "org.droidtv.amazonalexa.wizard.AlexaWizardActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.amazonalexa.wizard.AlexaWizardActivity-org.droidtv.amazonalexa",
+            "type": "app",
+        },
+        {
+            "label": "TV-guide",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.channels",
+                    "className": "org.droidtv.channels.ChannelsActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.channels.ChannelsActivity-org.droidtv.channels",
+            "type": "app",
+        },
+        {
+            "label": "Media",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.contentexplorer",
+                    "className": "org.droidtv.contentexplorer.MainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.contentexplorer.MainActivity-org.droidtv.contentexplorer",
+            "type": "app",
+        },
+        {
+            "label": "Demo Me",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.demome",
+                    "className": "org.droidtv.demome.DemoMeOptionsActivity",
+                },
+                "action": "empty",
+            },
+            "order": 0,
+            "id": "org.droidtv.demome.DemoMeOptionsActivity-org.droidtv.demome",
+            "type": "app",
+        },
+        {
+            "label": "Philips TV-samling",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.nettv.market",
+                    "className": "org.droidtv.nettv.market.MarketMainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.nettv.market.MarketMainActivity-org.droidtv.nettv.market",
+            "type": "app",
+        },
+        {
+            "label": "Toppval",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.nettvrecommender",
+                    "className": "org.droidtv.nettvrecommender.NetTvRecommenderMainActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.nettvrecommender.NetTvRecommenderMainActivity-org.droidtv.nettvrecommender",
+            "type": "app",
+        },
+        {
+            "label": "TV",
+            "intent": {
+                "component": {
+                    "packageName": "org.droidtv.playtv",
+                    "className": "org.droidtv.playtv.PlayTvActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "org.droidtv.playtv.PlayTvActivity-org.droidtv.playtv",
+            "type": "app",
+        },
+        {
+            "label": "Viafree",
+            "intent": {
+                "component": {
+                    "packageName": "se.viafree.android",
+                    "className": "com.viafree.android.SplashActivity",
+                },
+                "action": "android.intent.action.MAIN",
+            },
+            "order": 0,
+            "id": "com.viafree.android.SplashActivity-se.viafree.android",
+            "type": "app",
+        },
+    ],
+}
+
 ACTIVITIES_CURRENT = {
     "component": {
         "packageName": "org.droidtv.nettv.market",
@@ -46,11 +234,7 @@ ACTIVITIES_CURRENT = {
     }
 }
 
-ACTIVITIES_TV = {
-    "channel": {
-        "ccid": 1648
-    }
-}
+ACTIVITIES_TV = {"channel": {"ccid": 1648}}
 
 CHANNELDB_TV_CHANNELLISTS_ALL = {
     "id": "all",
