24a0a5a73f0d7e1823bb099dd3db436bede6235b
Joakim Plate <elupus@ecce.se>
2021-03-17 22:55:18 +0100

    Bump to 2.7.0 with auth exceptions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 PACKAGE_NAME = 'ha-philipsjs'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '2.6.1'
+VERSION = '2.7.0'
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'ccu', 'build'])
 
