717445c8367dbbbee1b4b1fe85895fc71faa8307
Florian Völker <github@floyer.de>
2023-06-13 23:54:19 +0200

    Added protocol parameter to getReq

--- haphilipsjs/__init__.py
+++ haphilipsjs/__init__.py
@@ -556,17 +556,16 @@ class PhilipsTV(object):
         if protocol is None:
             protocol = self.protocol
 
-        if self.protocol == "https":
+        if protocol == "https":
             port = HTTPS_PORT
         else:
             port = HTTP_PORT
 
         return f"{protocol}://{self._host}:{port}/{self.api_version}/{path}"
 
-    async def getReq(self, path) -> Optional[Dict]:
-
+    async def getReq(self, path, protocol = None) -> Optional[Dict]:
         try:
-            resp = await self.session.get(self._url(path))
+            resp = await self.session.get(self._url(path, protocol = protocol))
             if resp.status_code == 401:
                 raise AutenticationFailure("Authenticaion failed to device")
 
@@ -752,8 +751,7 @@ class PhilipsTV(object):
         orig_protocol = self.protocol
         
         for prot in protocols:
-            self.protocol = prot
-            r = cast(Optional[SystemType], await self.getReq("system"))
+            r = cast(Optional[SystemType], await self.getReq("system", protocol=prot))
             if r:
                 self.system = self._decodeSystem(r)
                 self.name = r.get("name")
