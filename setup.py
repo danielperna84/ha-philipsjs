b76edb17668cad86af7c37f9d02adb556232a19c
Joakim Plate <elupus@ecce.se>
2024-05-21 22:50:49 +0200

    Bump version t 3.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 PACKAGE_NAME = 'ha-philipsjs'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '3.1.1'
+VERSION = '3.2.0'
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'ccu', 'build'])
 
