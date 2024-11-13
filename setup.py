6a7a93e4b0de705cb5c4f06cd53d80ac0da31d73
Joakim Plate <elupus@ecce.se>
2024-11-13 18:59:45 +0100

    Update version to 3.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 PACKAGE_NAME = 'ha-philipsjs'
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = '3.2.2'
+VERSION = '3.2.3'
 
 PACKAGES = find_packages(exclude=['tests', 'tests.*', 'dist', 'ccu', 'build'])
 
